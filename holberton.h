d2a4c1a5742d440cae7b03cc966c5b6c16f9fee6
aslebloas <aslebloas@gmail.com>
2018-08-20 23:02:17 +0000

    Prototype of shell

--- holberton.h
+++ holberton.h
@@ -2,8 +2,12 @@
 #define _HOLBERTON_H_
 /* Libraries */
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /* prototypes */
 
