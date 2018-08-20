88598ce65b0378c7b1b905139ae537e93f3be15b
aslebloas <aslebloas@gmail.com>
2018-08-20 20:53:33 +0000

    get user command

--- simple_shell.c
+++ simple_shell.c
@@ -7,8 +7,25 @@
 
 int main(void)
 {
-	/* Display a prompt and wait for the user to type a command */
-	write(STRDIN_FILENO, "#cisfun$ ", 9);
+	int read_c; /* number of bytes read */
+	size_t nbytes = 200; /* number of bytes for the stream */
+	char *string; /* command line */
+
+	/* Display a prompt */
+	write(STDIN_FILENO, "#cisfun$ ", 9);
+
+	/* wait for the user to type a command */
+	string = malloc(nbytes + 1);
+	if (string == NULL)
+		return (1);
+
+	/* get user input */
+	read_c = getline(&string, &nbytes, stdin);
+	if (read_c == -1)
+	{
+		exit(90);
+		printf("getline() failed");
+	}
 
 	return (0);
 }
