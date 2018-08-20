278f3d211f302102498b615f4fbb823f788d787e
aslebloas <aslebloas@gmail.com>
2018-08-20 21:03:07 +0000

    handle the “end of file” condition

--- simple_shell.c
+++ simple_shell.c
@@ -7,24 +7,29 @@
 
 int main(void)
 {
-	int read_c; /* number of bytes read */
+	int read_c = 0; /* number of bytes read */
 	size_t nbytes = 200; /* number of bytes for the stream */
 	char *string; /* command line */
 
-	/* Display a prompt */
-	write(STDIN_FILENO, "#cisfun$ ", 9);
-
-	/* wait for the user to type a command */
-	string = malloc(nbytes + 1);
-	if (string == NULL)
-		return (1);
-
-	/* get user input */
-	read_c = getline(&string, &nbytes, stdin);
-	if (read_c == -1)
+	/* Programs runs until Ctrl+D is pressed */
+	while (read_c != EOF)
 	{
-		exit(90);
-		printf("getline() failed");
+		/* Display a prompt */
+		write(STDIN_FILENO, "#cisfun$ ", 9);
+
+		/* wait for the user to type a command */
+		string = malloc(nbytes + 1);
+		if (string == NULL)
+			return (1);
+
+		/* get user input */
+		read_c = getline(&string, &nbytes, stdin);
+		if (read_c == -1)
+		{
+			exit(90);
+			printf("getline() failed");
+		}
+
 	}
 
 	return (0);
