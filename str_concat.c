4c8f77503747d54058bb352502c3139135f1abb5
aslebloas <aslebloas@gmail.com>
2018-08-22 23:12:17 +0000

    Add str_concat function

---
+++ str_concat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string parameter
+ * @s2: string parameter
+ *
+ * Return: pointer to a char or NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int n, n1, n2;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	n1 = 0;
+	while (s1[n1] != '\0')
+		n1++;
+
+	n2 = 0;
+	while (s2[n2] != '\0')
+		n2++;
+
+	n = n1 + n2;
+
+	p = malloc((n + 1) * sizeof(char));
+
+	/* if malloc fails, return NULL */
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
+
+	for (j = 0; s2[j] != '\0'; j++)
+		p[n1 + j] = s2[j];
+
+	p[n1 + j] = '\0';
+
+	return (p);
+}
