22fb211314f2e360574c8dbe6e1efa6cf01fc786
aslebloas <aslebloas@gmail.com>
2018-08-23 00:59:09 +0000

    Add tok_path function

---
+++ tok_path.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * tok_path - stores each directory from path into an array
+ *
+ * Return: address of the array
+ */
+
+char **tok_path(void)
+{
+	char *path, *path_cpy, *token;
+	char **path_array;
+	int i, token_c = 0;
+
+	/* Initialize */
+	path = _getenv("PATH");
+	path_cpy = _strdup(path);
+
+	/* Find memory size for array */
+	token = strtok(path_cpy, ":");
+	while (token)
+	{
+		token = strtok(NULL, ":");
+		token_c++;
+	}
+
+	/* Allocate memory for array */
+	path_array = malloc(sizeof(char) * (token_c + 1));
+	if (path_array == NULL)
+		return (NULL);
+
+	/* Create tokens */
+	token_c = 0;
+	token = strtok(path, ":");
+	while (token)
+	{
+		++token_c;
+		path_array[token_c - 1] = token;
+		token = strtok(NULL, ":");
+	}
+	path_array[token_c] = 0;
+
+	return (path_array);
+}
