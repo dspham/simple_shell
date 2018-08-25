c37bd2497050a509c453f7d8476f828d4661a620
dspham <dsvpham@gmail.com>
2018-08-25 00:34:11 -0700

    updated betty styling

--- builtins.c
+++ builtins.c
@@ -1,26 +1,27 @@
 #include "holberton.h"
+
 /**
- * print_env - print the current environment
- * @env: environment
+ * printenviron - print the current environment
+ * @environ: environment
  * Return: none
  */
 void printenviron(char **environ)
 {
 	int i;
+
 	for (i = 0; environ[i] != NULL; i++)
-    {
-        write(1, environ[i], strlen(environ[i]));
-        write(1, "\n", 1);
-    }
+	{
+		write(1, environ[i], strlen(environ[i]));
+		write(1, "\n", 1);
+	}
 }
 
-
 /**
- * _exit - exit a program
- * @status:
+ * __exit - exit a program
+ * @status: status sent back to parent process
  * Return: none
  */
 int __exit(int status)
 {
-    exit(status);
+	exit(status);
 }
